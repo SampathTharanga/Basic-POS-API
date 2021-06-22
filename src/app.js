5c5acaa32f2dc41a4413c614b2768f300c28841e
Sampath Tharanga <sampaththaranga13@gmail.com>
2021-06-22 10:37:28 +0530

    Update read me file with some important notes.

--- src/app.js
+++ src/app.js
@@ -1,36 +1,16 @@
-/**
- * npm init # This will trigger the initialization 
- * npm init --yes # This will trigger automatically populated initialization. 
- * npm install <module> # Where <module> is the name of the module you want to install 
- * npm i <module> # Where <module> is the name of the module you want to install - using the i alias for installation 
- * npm install <module> --save # Install Modules and Save Them to Your package.json as a Dependency
- * npm install <module> --save-dev # Install Modules and Save Them to Your package.json as a Developer Dependency
- * npm install <module> --global # Where <module> is the name of the module you want to install globally 
- * npm install <module> -g # Where <module> is the name of the module you want to install globally, using the -g alias 
- * 
- * 
- * 
- * 
- * npm init -y
- * npm i express mysql body-parser --save
- * npm i nodemon --save-dev     <=OR YOU CAN USE BELOW COMMAND
- * npm i -D nodemon
- * npm i dotenv
- * 
- * 
- * NOTE: As of npm 5, the --save option is no longer necessary. If you do "npm install my-package", it will add my-package as a dependency in your package.json file. 
- * 
- * 
- * devDependencies should contain packages which are used during development or which are used to build your bundle, for example, mocha, jscs, grunt-contrib-watch, gulp-jade and etc. These packages are neseccery only while you are developing your project, also ESlint is used to check everything during building your bundle.
- */
-
-
 const express = require('express')
 const app = express()
 
 require('dotenv').config()
 
 const port = process.env.DB_PORT || 3000
+
+
+
+
+
+
+
 
 //LISTEN TO THE PORT
 app.listen(port, () => { console.log(`Server is runing on port ${port}.`)})
